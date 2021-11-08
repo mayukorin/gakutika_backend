9f049a50eefd7933c266237175f0b4f0a839b1d4
mayukorin <mayukuma@r.sea.sannet.ne.jp>
2021-11-08 17:08:02 +0900

    Add debug

--- app/services/user_authenticator.rb
+++ app/services/user_authenticator.rb
@@ -8,9 +8,11 @@ class UserAuthenticator
             @request_headers = request_headers
             puts token
             payload, = TokenDecryptor.call(token)
+            puts "llllllll"
             puts payload['user_id']
             user = User.find(payload['user_id'])
         rescue StandardError
+            puts "ppppppp"
             nil
         end
     end
