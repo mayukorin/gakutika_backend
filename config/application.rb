aae4d5b597b99978876c50dbba03f37748829588
mayukorin <mayukuma@r.sea.sannet.ne.jp>
2021-11-08 13:52:10 +0900

    Add session Controller

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,7 @@ require "rails/test_unit/railtie"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+require_relative '../lib/exception.rb'
 
 module GakutikaBackend
   class Application < Rails::Application
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   namespace :api do
-    get 'users/create'
+    resources :users, only: [:create]
+    post '/signin', to: 'sessions#create'
+    get '/me', to: 'sessions#me'
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
