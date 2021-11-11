d00b40f6e256cf7aeaaf4ebb1da5fabd1cebadaf
mayukorin <mayukuma@r.sea.sannet.ne.jp>
2021-11-11 23:48:23 +0900

    Add tough rank column to gakutika

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_09_124138) do
+ActiveRecord::Schema.define(version: 2021_11_11_144543) do
 
   create_table "gakutikas", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2021_11_09_124138) do
     t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "tough_rank"
     t.index ["user_id"], name: "index_gakutikas_on_user_id"
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,4 +7,7 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 user = User.find(1)
-user.gakutikas.create!(title: '塾生新聞会での新聞配布', content: '新聞を2月に配布');
+# user.gakutikas.create!(title: '塾生新聞会での新聞配布', content: '新聞を2月に配布');
+user.gakutikas.create!(title: '塾生新聞会での新聞配布2', content: '新聞を2月に配布');
+user.gakutikas.create!(title: '塾生新聞会での新聞配布3', content: '新聞を2月に配布');
+user.gakutikas.create!(title: '塾生新聞会での新聞配布4', content: '新聞を2月に配布');
