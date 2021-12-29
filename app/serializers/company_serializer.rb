853dfb6a798290de98111ef4cdfbe1e1cc90df14
mayukorin <mayukuma@r.sea.sannet.ne.jp>
2021-12-29 23:53:40 +0900

    Modify company serializer

--- app/serializers/company_serializer.rb
+++ app/serializers/company_serializer.rb
@@ -1,9 +1,15 @@
 class CompanySerializer < ActiveModel::Serializer
     attribute :name
     attribute :user_and_company, serializer: UserAndCompanySerializer
+    attribute :signin_user_and_company_and_gakutikas
 
     def user_and_company
+        user_and_company = UserAndCompany.eager_load(:user_and_company_and_gakutikas).find_by(user_id: @instance_options[:user_id], company_id: object.id)
+    end
+
+    def signin_user_and_company_and_gakutikas
         user_and_company = UserAndCompany.find_by(user_id: @instance_options[:user_id], company_id: object.id)
+        return user_and_company.user_and_company_and_gakutikas
     end
     
 end
