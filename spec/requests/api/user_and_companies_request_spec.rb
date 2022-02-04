81550601dae76d650304bb6bc56d6efb95c5c193
mayukorin <mayukuma@r.sea.sannet.ne.jp>
2022-02-04 23:22:00 +0900

    Add user_and_company update test

--- spec/requests/api/user_and_companies_request_spec.rb
+++ spec/requests/api/user_and_companies_request_spec.rb
@@ -38,7 +38,42 @@ RSpec.describe "Api::UserAndCompanies", type: :request do
           expect(Question.count).to match(0)
         end
       end
+
+      
     end
+
+    describe "#update" do
+      context "通常" do
+        let!(:user) do
+          FactoryBot.create(:user)
+        end
+        let!(:company) do
+            FactoryBot.create(:company)
+        end
+        let!(:user_and_company) do
+            UserAndCompany.create(company_id: company.id, user_id: user.id)
+        end
+        let!(:token) do
+            exp = Time.now.to_i + 4 * 60 
+            TokenProvider.new.call(user_id: user.id, exp: exp)
+        end
+        let!(:gakutika) do
+            user.gakutikas.create(title: "aaaaaa", content: "bbbbbbbbbbbbbb", tough_rank: 1, start_month: Date.new(2017,9,7), end_month: Date.new(2017,10,7))
+        end
+        let(:user_and_company_and_gakutika) do
+            UserAndCompanyAndGakutika.create(gakutika_id: gakutika.id, user_and_company_id: user_and_company.id)
+        end
+        let!(:question) do
+          gakutika.questions.create(query: "質問内容", answer: "解答", company_id: company.id, day: Date.new(2021, 11, 4))
+        end
+        it 'status accepted を返す' do
+          patch api_user_and_company_path(user_and_company.id), headers: { "Authorization" => "JWT " + token }, params: { user_and_company: { company_name: "abc" } }
+          expect(response).to have_http_status(:accepted)
+          expect(user_and_company.reload.company.name).to match("abc")
+        end
+      end  
+    end
+
   end
 
 end
