acab6d584ea8a584c81d4e82fc1330920a3bdc2c
mayukorin <mayukuma@r.sea.sannet.ne.jp>
2022-02-11 16:06:06 +0900

    Add optional true to company_id of user_and_company_table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_11_101945) do
+ActiveRecord::Schema.define(version: 2022_02_10_170331) do
 
   create_table "companies", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "name"
@@ -45,7 +45,7 @@ ActiveRecord::Schema.define(version: 2021_12_11_101945) do
 
   create_table "user_and_companies", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.bigint "user_id", null: false
-    t.bigint "company_id", null: false
+    t.bigint "company_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["company_id"], name: "index_user_and_companies_on_company_id"
