8359fc3732ae8bc870dc92d5e1948de16fbf30d7
mayukorin <mayukuma@r.sea.sannet.ne.jp>
2022-02-27 21:51:25 +0900

    refactor: Changed the hash variable name for clarity.

--- app/serializers/user_and_company_serializer.rb
+++ app/serializers/user_and_company_serializer.rb
@@ -16,12 +16,12 @@ class UserAndCompanySerializer < ActiveModel::Serializer
     def user_and_company_and_particular_gakutika
         unless @instance_options[:gakutika_id].nil? 
            # 余計な クエリ発行をさける
-           normal_hash = {}
+           user_and_company_and_particular_gakutika = {}
             object.user_and_company_and_gakutikas.each do |user_and_company_and_gakutika| 
                 if user_and_company_and_gakutika.gakutika_id == @instance_options[:gakutika_id] 
-                    normal_hash[:id] = user_and_company_and_gakutika.id
-                    normal_hash[:questions] = user_and_company_and_gakutika.questions
-                    return normal_hash
+                    user_and_company_and_particular_gakutika[:id] = user_and_company_and_gakutika.id
+                    user_and_company_and_particular_gakutika[:questions] = user_and_company_and_gakutika.questions
+                    return user_and_company_and_particular_gakutika
                 end
             end
         end
