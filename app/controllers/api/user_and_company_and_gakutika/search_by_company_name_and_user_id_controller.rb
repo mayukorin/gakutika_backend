d3b4253fe5a1d7c3ef331c5d2aa3717637150c32
mayukorin <mayukuma@r.sea.sannet.ne.jp>
2022-04-03 22:02:53 +0900

    fix: apply ambiguous search

--- app/controllers/api/user_and_company_and_gakutika/search_by_company_name_and_user_id_controller.rb
+++ app/controllers/api/user_and_company_and_gakutika/search_by_company_name_and_user_id_controller.rb
@@ -4,7 +4,7 @@ class Api::UserAndCompanyAndGakutika::SearchByCompanyNameAndUserIdController < A
     include ExceptionHandler
 
     def index
-        @company_id_list = Company.where("name like ?", "#{params[:name]}%").pluck(:id)
+        @company_id_list = Company.where("name like ?", "%#{params[:name]}%").pluck(:id)
         puts params[:name]
         puts @company_id_list
         @user_and_company_id_list = UserAndCompany.where(user_id: signin_user(request.headers).id, company_id: @company_id_list).pluck(:id)
